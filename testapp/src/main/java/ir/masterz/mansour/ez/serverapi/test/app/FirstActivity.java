3710b55b2f1297dccedfed13c11878161903bba7
Seyed Mansour Mirbehbahani <sm.mirbehbahani@gmail.com>
2021-06-11 10:04:44 +0430

    added Test App Small fixes

---
+++ testapp/src/main/java/ir/masterz/mansour/ez/serverapi/test/app/FirstActivity.java
@@ -0,0 +1,14 @@
+package ir.masterz.mansour.ez.serverapi.test.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
---
+++ testapp/src/main/java/ir/masterz/mansour/ez/serverapi/test/app/G.java
@@ -0,0 +1,32 @@
+package ir.masterz.mansour.ez.serverapi.test.app;
+import android.app.Application;
+import android.content.Context;
+import android.widget.Toast;
+
+import ir.masterz.mansour.ez.serverapi.ServerApi;
+
+
+public class G extends Application {
+
+    public static Context APP_CONTEXT;
+    public static ServerApi API;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        APP_CONTEXT = getApplicationContext();
+
+        API = new ServerApi(APP_CONTEXT);
+        API.getConfig().setLogging(true);
+        API.getConfig().setLoggingTag("malmal_log");
+
+    }
+
+    public static void message(String msg) {
+        Toast.makeText(APP_CONTEXT, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
+}
